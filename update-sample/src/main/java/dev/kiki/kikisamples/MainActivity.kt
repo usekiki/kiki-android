0ff9b4e1cacd66826a91e6797aa0d9e7a65dc415
Ali Nasrabadi <nasrabadiam@cafebazaar.ir>
2022-12-06 17:15:51 +0330

    Add update with ui

--- update-sample/src/main/java/dev/kiki/kikisamples/MainActivity.kt
+++ update-sample/src/main/java/dev/kiki/kikisamples/MainActivity.kt
@@ -1,5 +1,44 @@
 package dev.kiki.kikisamples
 
+import android.os.Bundle
+import android.util.Log
+import android.widget.Button
 import androidx.appcompat.app.AppCompatActivity
+import dev.kiki.update.AppUpdateManager
+import dev.kiki.update.UpdateManager
 
-class MainActivity : AppCompatActivity()
+class MainActivity : AppCompatActivity() {
+
+    private val updateManager: UpdateManager = AppUpdateManager
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        findViewById<Button>(R.id.updateWithUi).setOnClickListener {
+            handleUpdateWithUi()
+        }
+    }
+
+    private fun handleUpdateWithUi() {
+        updateManager.checkUpdateInfo {
+            onSuccess { updateInfo ->
+                if (updateInfo.isUpdateAvailable(this@MainActivity)) {
+                    updateManager.startUpdateFlow()
+                } else {
+                    Log.w(
+                        TAG,
+                        "No Update Available! latest available version = ${updateInfo.availableVersionCode}"
+                    )
+                }
+            }
+            onFailed { throwable ->
+                Log.w(TAG, "Error Happened in getting update info -> $throwable")
+            }
+        }
+    }
+
+    companion object {
+
+        private const val TAG = "UPDATE"
+    }
+}
