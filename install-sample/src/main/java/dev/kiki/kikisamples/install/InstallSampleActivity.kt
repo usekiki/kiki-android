4c711cebabf7942714e20f6ec079d0e11b9a33aa
Ali Nasrabadi <nasrabadiam@cafebazaar.ir>
2022-12-20 15:57:56 +0330

    Add start update flow with ui

--- install-sample/src/main/java/dev/kiki/kikisamples/install/InstallSampleActivity.kt
+++ install-sample/src/main/java/dev/kiki/kikisamples/install/InstallSampleActivity.kt
@@ -1,5 +1,45 @@
 package dev.kiki.kikisamples.install
 
+import android.os.Bundle
+import android.util.Log
+import android.widget.Button
 import androidx.appcompat.app.AppCompatActivity
+import dev.kiki.install.AppInstallManager
+import dev.kiki.install.InstallManager
 
-class InstallSampleActivity : AppCompatActivity()
+class InstallSampleActivity : AppCompatActivity() {
+
+    private val installManager: InstallManager = AppInstallManager
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_sample_install)
+        findViewById<Button>(R.id.installWithUi).setOnClickListener {
+            handleInstallWithUi()
+        }
+    }
+
+    private fun handleInstallWithUi() {
+        installManager.checkUpdateInfo {
+            onSuccess { updateInfo ->
+                if (updateInfo.isUpdateAvailable(this@InstallSampleActivity)) {
+                    installManager.startUpdateFlow(updateInfo)
+                } else {
+                    Log.w(
+                        TAG,
+                        "No Update Available! latest available version = " +
+                                "${updateInfo.availableVersionCode}"
+                    )
+                }
+            }
+            onFailed { throwable ->
+                Log.w(TAG, "Error Happened in getting update info -> $throwable")
+            }
+        }
+    }
+
+    companion object {
+
+        private const val TAG = "INSTALL"
+    }
+}
